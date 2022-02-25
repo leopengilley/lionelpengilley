a980d16bff3745aa090f3d4913b330017a8c8a4b
leopengilley <you@example.comlionel.pengilley@hotmail.com>
2022-02-25 13:43:36 +1100

    responsive design 2

--- src/pages/project.js
+++ src/pages/project.js
@@ -9,82 +9,62 @@ export default function Project() {
   return (
     <div className='grid-item' id='#projects'>
 
-        <div className='projectBoxStart'>
-          <h1 className='projTitle'>Projects</h1>
-        </div>
+        <h1 className='projTitle'>Projects</h1>
 
         <div class="projectContainer">
           <div className='projectBox'>
-            <img src={ Ttt } alt="tictactoe project"/>
-            <div class='projectContent'>
-              <div className='projectDescription'>
-                <h3>Tic-tac-toe</h3>
-                <p>Stack: JavaScript, HTML & CSS.</p>
-              </div>
-              <div className="links">
-                <a class="button primary" href="https://leopengilley.github.io/tictactoe/" target="_blank">
+            <img class="data" src={ Ttt } alt="tictactoe project"/>
+            <h3>Tic-tac-toe</h3>
+            <p>Stack: JavaScript, HTML & CSS.</p>
+            <div className="links">
+              <a class="button primary" href="https://leopengilley.github.io/tictactoe/" target="_blank">
+              Play
+              </a>
+              <a class="button" href="https://github.com/leopengilley/tictactoe" target="_blank">
+                GitHub
+              </a>
+            </div>
+          </div>
+
+          <div className='projectBox'>
+            <img class="data" src={ Drumpad } alt="drumpad project"/>
+            <h3>PLT-Drum-9</h3>
+            <p>Stack: React, Ruby on Rails, HTML & CSS.</p>
+            <div className="links">
+              <a class= "button primary" href="https://plt-drum-9.herokuapp.com/" target="_blank">
                 Play
-                </a>
-                <a class="button" href="https://github.com/leopengilley/tictactoe" target="_blank">
-                  GitHub
-                </a>
-              </div>
+              </a>
+              <a class= "button" href="https://github.com/trentthom/dm" target="_blank">
+                GitHub
+              </a>
             </div>
           </div>
 
           <div className='projectBox'>
-            <img src={ Drumpad } alt="drumpad project"/>
-            <div class='projectContent'>
-              <div className='projectDescription'>
-                <h3>PLT-Drum-9</h3>
-                <p>Stack: React, Ruby on Rails, HTML & CSS.</p>
-              </div>
-              <div className="links">
-                <a class= "button primary" href="https://plt-drum-9.herokuapp.com/" target="_blank">
-                  Play
-                </a>
-                <a class= "button" href="https://github.com/trentthom/dm" target="_blank">
-                  GitHub
-                </a>
-              </div>
+            <img class="data" src={ Couscous } alt="game project"/>
+            <h3>Glowing Couscous</h3>
+            <p>Stack: Phaser, JavaScript, HTML & CSS.</p>
+            <div className="links">
+              <a class= "button primary" href="https://leopengilley.github.io/glowing-couscous/" target="_blank">
+                Play
+              </a>
+              <a class= "button" href="https://github.com/leopengilley/glowing-couscous" target="_blank">
+                GitHub
+              </a>
             </div>
           </div>
 
           <div className='projectBox'>
-            <img src={ Couscous } alt="game project"/>
-            <div class='projectContent'>
-              <div className='projectDescription'>
-                <h3>Glowing Couscous</h3>
-                <p>Stack: Phaser, JavaScript, HTML & CSS.</p>
-              </div>
-              <div className="links">
-                <a class= "button primary" href="https://leopengilley.github.io/glowing-couscous/" target="_blank">
-                  Play
-                </a>
-                <a class= "button" href="https://github.com/leopengilley/glowing-couscous" target="_blank">
-                  GitHub
-                </a>
-              </div>
-            </div>
-          </div>
-
-          <div className='projectBox'>
-            <div className='projectPic'>
-              <img src={ Pokemon } alt="pokemon project"/>
-            </div>
-            <div class='projectContent'>
-              <div className='projectDescription'>
-                <h3>Pokedex</h3>
-                <p>Stack: Javascript, Ruby on Rails, HTML & CSS.</p>
-              </div>
-              <div className="links">
-                <a class= "button primary" href="https://stark-reaches-80757.herokuapp.com/" target="_blank">
-                  Play
-                </a>
-                <a class= "button" href="https://github.com/leopengilley/potential-octo-adventure" target="_blank">
-                  GitHub
-                </a>
-              </div>
+            <img class="data" src={ Pokemon } alt="pokemon project"/>
+            <h3>Pokedex</h3>
+            <p>Stack: Javascript, Ruby on Rails, HTML & CSS.</p>
+            <div className="links">
+              <a class= "button primary" href="https://stark-reaches-80757.herokuapp.com/" target="_blank">
+                Play
+              </a>
+              <a class= "button" href="https://github.com/leopengilley/potential-octo-adventure" target="_blank">
+                GitHub
+              </a>
             </div>
           </div>
         </div>
